f70d635595fc908b77ae62d5541d5094187e3717
Igor Rak <igorrakfed@gmail.com>
2022-06-29 22:00:52 +0200

    images

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserView, MobileView } from 'react-device-detect'
 
 import { Context } from '../../index'
+import jpg from '../../assets/image/jpg.jpg'
+import png from '../../assets/image/png.png'
+import svg_img from '../../assets/image/svg.svg'
 
 const Start = () => {
   const { store } = React.useContext(Context)
@@ -12,6 +15,9 @@ const Start = () => {
         <h1>This is rendered only in browser</h1>
         <h1>Test</h1>
         <div>store.test: {store.test}</div>
+        <img src={jpg} alt="" />
+        <img src={png} alt="" />
+        <img src={svg_img} alt="" />
       </BrowserView>
       <MobileView>
         <h1>This is rendered only on mobile</h1>
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  // entry: path.resolve(__dirname, '..', './src/index.tsx'),
   entry: {
     main: path.resolve(__dirname, '..', './src/index.tsx'),
     second: './src/index.js',
@@ -45,7 +44,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '..', './dist'),
-    filename: '[name].[contenthash].js',  
+    filename: '[name].[contenthash].js',
   },
 
   plugins: [
