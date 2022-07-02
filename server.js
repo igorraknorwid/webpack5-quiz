e7f4c7d247ffe23acee16c6be67d410920bfef7a
Igor Rak <igorrakfed@gmail.com>
2022-07-02 06:52:24 +0200

    regex test 6

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 // app.use(/(\/[a-z]+)\/\d+|(\/[a-z]+\/)|(\/[a-z]+)/g,express.static(path.join(__dirname, 'dist')))
-// app.use(/\/[a-z]+)\/\d+/,express.static(path.join(__dirname, 'dist')))
+app.use(/\/[a-z]+\/\d+/,express.static(path.join(__dirname, 'dist')))
 
 app.set('port', process.env.PORT || 8080)
 
