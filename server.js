7d8ada48fa356a4e1c12c724d295dedf704a51a6
Igor Rak <igorrakfed@gmail.com>
2022-07-02 06:12:54 +0200

    server regex route added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
+app.use(/(\/[a-z]+)\/\d+|(\/[a-z]+\/)|(\/[a-z]+)/g,express.static(path.join(__dirname, 'dist')))
+
 app.set('port', process.env.PORT || 8080)
 
 var server = app.listen(app.get('port'), function () {
