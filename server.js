ae5c89978aef5f8d22aa00cbb935004ef4095082
Igor Rak <igorrakfed@gmail.com>
2022-07-02 06:34:04 +0200

    regex test 1

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
-app.use(/(\/[a-z]+)\/\d+|(\/[a-z]+\/)|(\/[a-z]+)/g,express.static(path.join(__dirname, 'dist')))
+// app.use(/(\/[a-z]+)\/\d+|(\/[a-z]+\/)|(\/[a-z]+)/g,express.static(path.join(__dirname, 'dist')))
+app.use(/\/quiz/,express.static(path.join(__dirname, 'dist')))
 
 app.set('port', process.env.PORT || 8080)
 
